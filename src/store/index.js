93cc01d3ed3ff2fa1b1fe38851ab8ebd5e383749
CompileYouth <CompileYouth@163.com>
2017-02-22 23:06:00 +0800

    Add redux-devtools-extension.

--- src/store/index.js
+++ src/store/index.js
@@ -2,4 +2,6 @@ import {createStore, applyMiddleware} from 'redux';
 
 import reducers from '../reducers';
 
-export default createStore(reducers);
+const enhancer = process.env.NODE_ENV === 'production' ? require('./enhancer.js').default : require('./enhancer.dev.js').default;
+
+export default createStore(reducers, enhancer);
