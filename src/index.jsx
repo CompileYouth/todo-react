7616987b6458c2f2768261dbc6e7f6287077dd33
CompileYouth <CompileYouth@163.com>
2017-02-22 20:55:28 +0800

    Add redux, react-redux and redux-actions.

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+
+import store from './store';
 
 ReactDOM.render(
-  <div>Hello React!</div>,
+  <Provider store={store}>
+    Hello
+  </Provider>,
   document.getElementById('react-root')
 );
--- src/vendor.js
+++ src/vendor.js
@@ -1,2 +1,4 @@
 import 'react';
 import 'react-dom';
+import 'redux';
+import 'redux-actions';
