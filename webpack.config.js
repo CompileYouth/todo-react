a9f69331d3a107d4454748e96ed25dd47d244e9e
CompileYouth <CompileYouth@163.com>
2017-02-23 11:15:48 +0800

    - Add less - Add html to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,53 @@
 const path = require('path');
 const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: {
     vendor: './src/vendor.js',
-    main: './src/index.jsx'
+    td: ['./src/index.jsx', './src/res/index.less', './src/index.html']
   },
   output: {
-    filename: 'js/[name].js',
-    path: path.resolve(__dirname, 'public/assets'),
-    publicPath: '/assets'
+    filename: 'assets/js/[name].js',
+    path: path.resolve(__dirname, 'public'),
+    publicPath: '/'
   },
   devServer: {
     contentBase: path.resolve(__dirname, 'public')
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx'],
+    alias: {
+      'normalize.css': path.resolve(__dirname, './node_modules/normalize.css/normalize.css')
+    }
   },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         loader: 'babel-loader'
+      },
+      {
+        test: /\.less$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'less-loader']
+        })
+      },
+      {
+        test: /\.html$/,
+        use: [
+          'file-loader?[name].html',
+          'extract-loader',
+          'html-loader'
+        ]
       }
     ]
   },
   plugins: [
     new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor'
-    })
+    }),
+    new ExtractTextPlugin('assets/css/[name].css')
   ]
 };
