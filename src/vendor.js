1a9d3023f8175f6618dd13d0bef08acffc838191
CompileYouth <CompileYouth@163.com>
2017-02-27 11:03:04 +0800

    Modify vendor.js

--- src/vendor.js
+++ src/vendor.js
@@ -1,5 +1,11 @@
+import 'normalize.css';
+
+import 'classnames';
+import 'immutable';
+import 'material-ui';
 import 'react';
 import 'react-dom';
 import 'redux';
 import 'redux-actions';
 import 'react-tap-event-plugin';
+import 'react-motion';
